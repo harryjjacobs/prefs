114a9f1d130bae29fd97e86f66efa43d0b4e34ea
Harry Jacobs <harryjjacobs@googlemail.com>
2017-07-12 17:09:24 +0100

    modified:   prefs/__init__.py

--- prefs/__init__.py
+++ prefs/__init__.py
@@ -1,25 +1,33 @@
 PREFERENCES_PATH = 'preferences'
-import os.path
+import os
 
 def add(name, value, overwite_existing=False):
-	if not path.exists(PREFERENCES_PATH):
+	if not os.path.exists(PREFERENCES_PATH):
         	os.makedirs(PREFERENCES_PATH)
-	if path.isfile(path.join(PREFERENCES_PATH, name)) and not overwite_existing:
+	if os.path.isfile(os.path.join(PREFERENCES_PATH, name)) and not overwite_existing:
 		raise Exception('Preference already exists', '{0} has already been added. If you wish to overwrite it call with overwiteExisting=True or use the prefs.update method'.format(name))
 	else:
-		_save_to_file(path.join(PREFERENCES_PATH, name), value)
+		_save_to_file(os.path.join(PREFERENCES_PATH, name), value)
 
 def remove(name):
-	os.remove(path.join(PREFERENCES_PATH, name))
+	if os.path.exists(os.path.join(PREFERENCES_PATH, name)):
+		os.remove(os.path.join(PREFERENCES_PATH, name))
+	else:
+		raise Exception('Could not remove preference', 'No preference with name {0} was found'.format(name))
 		
 def get(name):
 	return _try_load_from_file(path.join(PREFERENCES_PATH, name))
 	
-def update(name, value, throw):
-	add(name, value, True)
+def update(name, value, create_if_nonexistent=False):
+	if os.path.exists(os.path.join(PREFERENCES_PATH, name)) or create_if_nonexistent:
+		add(name, value, True)
+	else:
+		raise Exception('Could not update preference', 'Preference does not exist and create_if_nonexistent set to False. Set it to True if you want to create the preference or use the add method to add it first.')
+
+
 
 def _try_load_from_file(name):
-	if path.isfile(name):
+	if os.path.isfile(name):
 		with open(name, 'r') as f:
 			return f.read()
 	else:
