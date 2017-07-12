ac898b46acc4e53400054d94c2e13d8097584a06
Harry Jacobs <harryjjacobs@googlemail.com>
2017-07-12 16:56:33 +0100

    modified:   prefs/__init__.py

--- prefs/__init__.py
+++ prefs/__init__.py
@@ -1,23 +1,25 @@
-PREFERENCES_PATH = 'prefs'
-import os
+PREFERENCES_PATH = 'preferences'
+import os.path
 
 def add(name, value, overwite_existing=False):
-	if os.path.isfile(name) and not overwite_existing:
+	if not path.exists(PREFERENCES_PATH):
+        	os.makedirs(PREFERENCES_PATH)
+	if path.isfile(path.join(PREFERENCES_PATH, name)) and not overwite_existing:
 		raise Exception('Preference already exists', '{0} has already been added. If you wish to overwrite it call with overwiteExisting=True or use the prefs.update method'.format(name))
 	else:
-		_save_to_file(name, value)
+		_save_to_file(path.join(PREFERENCES_PATH, name), value)
 
 def remove(name):
-	os.remove(name)
+	os.remove(path.join(PREFERENCES_PATH, name))
 		
 def get(name):
-	return _try_load_from_file(name)
+	return _try_load_from_file(path.join(PREFERENCES_PATH, name))
 	
 def update(name, value, throw):
 	add(name, value, True)
 
 def _try_load_from_file(name):
-	if os.path.isfile(name):
+	if path.isfile(name):
 		with open(name, 'r') as f:
 			return f.read()
 	else:
